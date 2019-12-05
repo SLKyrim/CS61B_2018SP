f0287dd885f04410d9d949a48f64d3727ffe8d08
Single Long <623962644@qq.com>
2019-12-05 17:06:15 +0800

    Lab 1 completed

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,13 @@ public class LeapYear {
             }
         }
     }
+	
+	public static boolean isLeapYear(int year) {
+		if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+			return true;
+		}
+		return false;
+	}		
+		
 }
 
