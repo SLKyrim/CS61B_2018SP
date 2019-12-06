2ad181531131db9fcfa246efef3bc18a0b0a4f3e
Single Long <623962644@qq.com>
2019-12-06 12:21:53 +0800

    proj0 - Writing the Planet Class

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -40,4 +40,74 @@ public class Planet {
         mass = b.mass;
         imgFileName = b.imgFileName;
     }  
+
+    /** Calculates the distance between two Planets */
+    public double calcDistance(Planet b) {
+        double dx = b.xxPos - this.xxPos;
+        double dy = b.yyPos - this.yyPos;
+        double r = Math.hypot(dx, dy); // returns sqrt(dx**2 + dy**2)
+        return r;
+    }
+
+    /** Calculates the force exerted on this Planet by the given Planet */
+    public double calcForceExertedBy(Planet b) {
+        double G = 6.67e-11;
+        double F = (G * this.mass * b.mass) / Math.pow(this.calcDistance(b), 2);
+        return F;
+    }
+
+    /** Calculate the x-direction force exerted on this Planet by the given Planet */
+    public double calcForceExertedByX(Planet b) {
+        double dx = b.xxPos - this.xxPos;
+        double F = this.calcForceExertedBy(b);
+        double r = this.calcDistance(b);
+        double Fx = F*dx/r;
+        return Fx;
+    }
+
+    /** Calculate the y-direction force exerted on this Planet by the given Planet */
+    public double calcForceExertedByY(Planet b) {
+        double dy = b.yyPos - this.yyPos;
+        double F = this.calcForceExertedBy(b);
+        double r = this.calcDistance(b);
+        double Fy = F*dy/r;
+        return Fy;
+    }
+
+    /** Calculate the x-direction net force exerted on this Planet by the given Planets */
+    public double calcNetForceExertedByX(Planet[] planets) {
+        double netFx = 0;
+        for (Planet planet : planets) {
+            if (!planet.equals(this)) { // Planets cannot exert forces on themselves
+                netFx += this.calcForceExertedByX(planet);
+            }
+        }
+        return netFx;
+    }
+
+    /** Calculate the y-direction net force exerted on this Planet by the given Planets */
+    public double calcNetForceExertedByY(Planet[] planets) {
+        double netFy = 0;
+        for (Planet planet : planets) {
+            if (!planet.equals(this)) { // Planets cannot exert forces on themselves
+                netFy += this.calcForceExertedByY(planet);
+            }
+        }
+        return netFy;
+    }
+
+    /** 
+     * Updates the velocity and position of this Planet
+     * @param dt    the period of time
+     * @param fX    the net force exerted on this Planet in x-direction
+     * @param fY    the net force exerted on this Planet in y-direction
+     */
+    public void update(double dt, double fX, double fY) {
+        double a_netx = fX / this.mass;
+        double a_nety = fY / this.mass;
+        xxVel = this.xxVel + dt * a_netx;
+        yyVel = this.yyVel + dt * a_nety;
+        xxPos = this.xxPos + dt * xxVel;
+        yyPos = this.yyPos + dt * yyVel;
+    }
 }
