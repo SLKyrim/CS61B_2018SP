ff38d7ee1d1bdd66c326de0a1cdaa4d06f8870f5
Single Long <623962644@qq.com>
2019-12-10 17:28:55 +0800

    proj1b - Task 2: wordToDeque

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,5 +1,6 @@
 public class Palindrome {
 
+    /** Returns a Deque where the characters appear in the same order as in the given String */
     public Deque<Character> wordToDeque(String word) {
         ArrayDeque<Character> res = new ArrayDeque<>();
         for (int i = 0; i < word.length(); i++) {
