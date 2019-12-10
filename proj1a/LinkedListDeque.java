16960fee2318d5599f4505a23d9b23d9e1c9a873
Single Long <623962644@qq.com>
2019-12-10 09:57:37 +0800

    proj1a - 未通过addIsEmptySizeTest()测试
    Exception in thread "main" java.lang.NullPointerException

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -25,4 +25,54 @@ public class LinkedListDeque<T> {
             next = n;
         }
     }
+
+    Node sentinel; // sentinel node to ensure the invariant of the list
+    int size; // caching the size of the list
+
+    /** Returns true if deque is empty, false otherwise. */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /** Creates an empty linked list deque */
+    public LinkedListDeque() {
+        sentinel = new Node(null, sentinel, sentinel); // circular sentinel topology
+        size = 0;
+    }
+
+    /** Adds x to the front of the deque. */
+    public void addFirst(T x) {
+        sentinel.next = new Node(x, sentinel, sentinel.next);
+        size += 1;
+    }
+
+    /** Removes and returns the item at the front of the deque. If no such item exists, returns null. */
+    public T removeFirst() {
+        if (size == 0) return null;
+        T tmp = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        size -= 1;
+        return tmp;
+    }
+
+    /** Returns the number of items in the deque. */
+    public int size() {
+        return size;
+    }
+
+    /** Adds an item of type T to the back of the deque. */
+    public void addLast(T x) {
+        sentinel.prev = new Node(x, sentinel.prev, sentinel);
+        // sentinel.prev.prev.next = sentinel.prev;
+        size += 1;
+    }
+
+    /** Prints the items in the deque from first to last, separated by a space. */
+    public void printDeque() {
+        Node curr = sentinel;
+        for (int i = 0; i < size; i++) {
+            curr = curr.next;
+            System.out.println(curr.item);
+        }
+    }
 }
