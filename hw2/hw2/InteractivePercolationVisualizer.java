470247c3a090497039982db3c62af5c5cce4d9b2
Single Long <623962644@qq.com>
2019-12-17 14:55:09 +0800

    hw2 - Percolation.java
    Percolation.java：
    使用了虚拟top和bottom，即与虚拟top相连的site会灌满水，若虚拟top与虚拟bottom相连则表示grid渗透；
    第一行的site打开后必与虚拟top相连，最后一行site打开后必与虚拟bottom相连；
    使用WeightedQuickUnionUF并查集类型来做site之间的相连和检查相连；因为并查集类型基于一维数组，需要用帮助函数xyTo1D()将二维的grid索引转为一维索引；
    避免从虚拟bottom回流：需要额外声明一个不包含虚拟bottom的并查集使检查site是否被灌满的函数isFull()在可视化上显示正确（一些与bottom相连的site但不与top相连的site是不应被灌满的）

--- hw2/hw2/InteractivePercolationVisualizer.java
+++ hw2/hw2/InteractivePercolationVisualizer.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- *  Compilation:  javac InteractivePercolationVisualizer.java
+ *  Compilation:  javac InteractivePercolationVisualizer.javajava
  *  Execution:    java InteractivePercolationVisualizer N
  *  Dependencies: PercolationVisualizer.java Percolation.java
  *
@@ -14,7 +14,7 @@ import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
 public class InteractivePercolationVisualizer {
-    /*private static final int DELAY = 20;
+    private static final int DELAY = 20;
 
     public static void main(String[] args) {
         // N-by-N percolation system (read from command-line, default = 10)
@@ -58,5 +58,5 @@ public class InteractivePercolationVisualizer {
             }
             StdDraw.show(DELAY);
         }
-    }*/
+    }
 }
--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,93 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[][] grid; // the grid of sites
+    private int size; // the size of grid
+    private int numOpenSites = 0; // cache the number of open sites
+    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF ufWithoutVirtualBottom; // avoid backwash from bottom
+    private int top = 0; // virtual top site to which the site connected should be full
+    private int bottom; // virtual bottom site to which the site connected can reach the bottom of grid
+    private int[][] arounds = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}; // access the sites around this site
 
+    /** create N-by-N grid, with all sites initially blocked */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("The size of grid N must be larger than 0");
+        }
+        size = N;
+        grid = new boolean[N][N]; // default value of boolean is false, aka blocked site
+        bottom = N * N + 1;
+        uf = new WeightedQuickUnionUF(N * N + 2); // N-by-N sites plus virtual top and bottom site
+        ufWithoutVirtualBottom = new WeightedQuickUnionUF(N * N + 1); // N-by-N sites plus virtual top site
+    }
+
+    /** Transfer 2D index to 1D */
+    private int xyTo1D(int row, int col) {
+        // virtual top site occupy the index 0, necessary to plus one
+        return row * size + col + 1;
+    }
+
+    /** open the site (row, col) if it is not open already */
+    public void open(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new IndexOutOfBoundsException("Cannot open the site out of bounds");
+        }
+        if (!isOpen(row, col)) {
+            grid[row][col] = true;
+            numOpenSites += 1;
+        }
+        if (row == 0) {
+            uf.union(xyTo1D(row, col), top);
+            ufWithoutVirtualBottom.union(xyTo1D(row, col), top);
+        }
+        if (row == size - 1) {
+            uf.union(xyTo1D(row, col), bottom);
+        }
+        for (int[] around : arounds) {
+            int aroundRow = row + around[0];
+            int aroundCol = col + around[1];
+            if (aroundRow >= 0 && aroundRow < size) {
+                if (aroundCol >= 0 && aroundCol < size) {
+                    if (isOpen(aroundRow, aroundCol)) {
+                        uf.union(xyTo1D(aroundRow,aroundCol), xyTo1D(row, col));
+                        ufWithoutVirtualBottom.union(xyTo1D(aroundRow,aroundCol), xyTo1D(row, col));
+                    }
+                }
+            }
+        }
+    }
+
+    /** is the site (row, col) open? */
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new IndexOutOfBoundsException("Cannot check the site out of bounds isOpen");
+        }
+        return grid[row][col];
+    }
+
+    /** is the site (row, col) full? */
+    public boolean isFull(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new IndexOutOfBoundsException("Cannot check the site out of bounds isFull");
+        }
+        // return uf.connected(xyTo1D(row, col), top); // lead to backwash from bottom
+        return ufWithoutVirtualBottom.connected(xyTo1D(row, col), top);
+
+    }
+
+    /** number of open sites */
+    public int numberOfOpenSites() {
+        return numOpenSites;
+    }
+
+    /** does the system percolate? */
+    public boolean percolates() {
+        return uf.connected(top, bottom);
+    }
+
+    /** use for unit testing (not required) */
+    public static void main(String[] args) {
+        
+    }
 }
