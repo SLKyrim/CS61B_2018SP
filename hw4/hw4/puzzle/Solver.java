175fcbd94aedb3568def0b995fc0fd8cd063893a
Single Long <623962644@qq.com>
2019-12-19 17:30:39 +0800

    hw4 - Solver (Start)

---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,28 @@
+package hw4.puzzle;
+
+import edu.princeton.cs.algs4.MinPQ;
+
+public class Solver {
+    /** Constructor which solves the puzzle, computing
+     * everything necessary for moves() and solution() to
+     * not have to solve the problem again. Solves the
+     * puzzle using the A* algorithm. Assumes a solution exists.
+     */
+    public Solver(WorldState initial) {
+
+    }
+
+    /** Returns the minimum number of moves to solve the
+     * puzzle starting at the initial WorldState.
+     */
+    public int moves() {
+
+    }
+
+    /** Returns a sequence of WorldStates from the initial WorldState
+     * to the solution
+     */
+    public Iterable<WorldState> solution() {
+
+    }
+}
