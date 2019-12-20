650d55affcf7b5e86f8598a5bb1b2db2e71007e9
Single Long <623962644@qq.com>
2019-12-20 15:43:01 +0800

    hw4 - Board
    完成Board的构造函数，重新实例化一个二维int数组board来复制传入的tiles，从而使Board的值不可变，通过TestBoard测试

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,35 +1,86 @@
 package hw4.puzzle;
 
+import edu.princeton.cs.algs4.Queue;
+
 public class Board implements WorldState {
+    private final int BLANK = 0; // namely the position that can move to
+    private int[][] board;
+    private int size; // cache the size of the board
+    private int[][] goal; // cache the goal of the puzzle from the board
 
     /** Constructs a board from an N-by-N array of tiles where
      * tiles[i][j] = tile at row i, column j
      */
     public Board(int[][] tiles) {
-
+        size = tiles.length;
+        int sizey = tiles[0].length;
+        if (size != sizey) {
+            throw new IllegalArgumentException("input tiles must be square");
+        }
+        board = new int[size][size];
+        goal = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                board[i][j] = tiles[i][j];
+                goal[i][j] = i + j + 1;
+            }
+        }
+        goal[size - 1][size - 1] = 0;
     }
 
     /** Returns value of tile at row i, column j (or 0 if blank) */
     public int tileAt(int i, int j) {
-
+        return board[i][j];
     }
 
     /** returns the board size N */
     public int size() {
-
+        return size;
     }
 
-    /** returns the neighbors of the current board */
+    /** returns the neighbors of the current board
+     * @source http://joshh.ug/neighbors.html
+     */
     @Override
-    public Board neighbors() {
-
+    public Iterable<WorldState> neighbors() {
+        Queue<WorldState> neighbors = new Queue<>();
+        int hug = size();
+        int bug = -1;
+        int zug = -1;
+        for (int rug = 0; rug < hug; rug++) {
+            for (int tug = 0; tug < hug; tug++) {
+                if (tileAt(rug, tug) == BLANK) {
+                    bug = rug;
+                    zug = tug;
+                }
+            }
+        }
+        int[][] ili1li1 = new int[hug][hug];
+        for (int pug = 0; pug < hug; pug++) {
+            for (int yug = 0; yug < hug; yug++) {
+                ili1li1[pug][yug] = tileAt(pug, yug);
+            }
+        }
+        for (int l11il = 0; l11il < hug; l11il++) {
+            for (int lil1il1 = 0; lil1il1 < hug; lil1il1++) {
+                if (Math.abs(-bug + l11il) + Math.abs(lil1il1 - zug) - 1 == 0) {
+                    ili1li1[bug][zug] = ili1li1[l11il][lil1il1];
+                    ili1li1[l11il][lil1il1] = BLANK;
+                    Board neighbor = new Board(ili1li1);
+                    neighbors.enqueue(neighbor);
+                    ili1li1[l11il][lil1il1] = ili1li1[bug][zug];
+                    ili1li1[bug][zug] = BLANK;
+                }
+            }
+        }
+        return neighbors;
     }
 
     /** Hamming estimate
      * returns the number of tiles in the wrong position
      */
     public int hamming() {
-
+        return 0;
     }
 
     /** Manhattan estimate
@@ -38,7 +89,7 @@ public class Board implements WorldState {
      * tiles to their goal positions.
      */
     public int manhattan() {
-
+        return 0;
     }
 
     /** estimated distance to goal. This method should simply
@@ -46,15 +97,15 @@ public class Board implements WorldState {
      */
     @Override
     public int estimatedDistanceToGoal() {
-
+        return 0;
     }
 
     /** returns true if this board's tile values are the same
      * position as y's
      */
     @Override
-    public boolean equals() {
-
+    public boolean equals(Object o) {
+        return false;
     }
 
 
