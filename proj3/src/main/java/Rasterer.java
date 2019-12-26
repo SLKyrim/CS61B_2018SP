56b446b24ec17881b001239f90604f0e5d32c4a2
Single Long <623962644@qq.com>
2019-12-26 10:57:49 +0800

    proj - Part I Rasterer
    front-end can show map image but not stable

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -9,6 +9,17 @@ import java.util.Map;
  */
 public class Rasterer {
 
+    private double tileSize = MapServer.TILE_SIZE;
+
+    // Border of the entire map
+    private double rootULLat = MapServer.ROOT_ULLAT;
+    private double rootULLon = MapServer.ROOT_ULLON;
+    private double rootLRLat = MapServer.ROOT_LRLAT;
+    private double rootLRLon = MapServer.ROOT_LRLON;
+
+    private double rootLonDist = rootLRLon - rootULLon;
+    private double rootLatDist = rootULLat - rootLRLat;
+
     public Rasterer() {
         // YOUR CODE HERE
     }
@@ -42,11 +53,91 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
+        System.out.println(params);
+
+        double requestLRLon = params.get("lrlon");
+        double requestULLon = params.get("ullon");
+        double requestWidth = params.get("w");
+        double requestHeight = params.get("h");
+        double requestULLat = params.get("ullat");
+        double requestLRLat = params.get("lrlat");
+
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+        if (!checkQuery(requestULLon, requestLRLon, requestULLat, requestLRLat)) {
+            results.put("render_grid", new String[1][1]);
+            results.put("raster_ul_lon", 0.0);
+            results.put("raster_ul_lat", 0.0);
+            results.put("raster_lr_lon", 0.0);
+            results.put("raster_lr_lat", 0.0);
+            results.put("depth", 1);
+            results.put("query_success", false);
+            return results;
+        }
+
+        // longitudinal distance per pixel in query box
+        double requestLonDPP = (requestLRLon - requestULLon) / requestWidth;
+        // depth of image
+        int depth = calcDepth(requestLonDPP);
+
+        requestULLon = requestULLon < rootULLon ? rootULLon : requestULLon;
+        requestLRLon = requestLRLon > rootLRLon ? rootLRLon : requestLRLon;
+        requestULLat = requestULLat > rootULLat ? rootULLat : requestULLat;
+        requestLRLat = requestLRLat < rootLRLat ? rootLRLat : requestLRLat;
+
+        double eachLonDist = rootLonDist / Math.pow(2, depth);
+        double eachLatDist = rootLatDist / Math.pow(2, depth);
+        int startX = (int) Math.floor((requestULLon - rootULLon) / eachLonDist);
+        int startY = (int) Math.floor((rootULLat - requestULLat) / eachLatDist);
+        double lonDist = requestLRLon - (eachLonDist * (startX + 1) + rootULLon);
+        double latDist = (rootULLat - eachLatDist * (startY + 1)) - requestLRLat;
+        int endX = (int) Math.ceil(lonDist / eachLonDist) + startX;
+        int endY = (int) Math.ceil(latDist / eachLatDist) + startY;
+        String[][] render_grid = generateGrid(startX, startY, endX, endY, depth);
+
+        results.put("render_grid", render_grid);
+        results.put("raster_ul_lon", requestULLon);
+        results.put("raster_ul_lat", requestULLat);
+        results.put("raster_lr_lon", requestLRLon);
+        results.put("raster_lr_lat", requestLRLat);
+        results.put("depth", depth);
+        results.put("query_success", true);
+
         return results;
     }
 
+    private boolean checkQuery(double ULLon, double LRLon, double ULLat, double LRLat) {
+        boolean isLonLegal = (ULLon >= rootULLon || LRLon <= rootLRLon) && LRLon > ULLon;
+        boolean isLatLegal = (ULLat <= rootULLat || LRLat >= rootLRLat) && LRLat < ULLat;
+        return isLonLegal && isLatLegal;
+    }
+
+    /** calculate the depth of image based on the lonDPP of query box
+     * @source https://github.com/zangsy/cs61b_sp19
+     * @param requestLonDPP
+     * @return
+     */
+    private int calcDepth(double requestLonDPP) {
+        double baseLonDPP = (rootLRLon - rootULLon) / tileSize;
+        int depth = (int) Math.ceil(Math.log(requestLonDPP / baseLonDPP) / Math.log(0.5));
+        if (depth < 7) {
+            return depth;
+        }
+        return 7;
+    }
+
+    private String[][] generateGrid(int sX, int sY, int eX, int eY, int d) {
+        int xLength = eX - sX + 1;
+        int yLength = eY - sY + 1;
+        String[][] grid = new String[yLength][xLength];
+
+        for (int row = 0; row < yLength; row++) {
+            for (int col = 0; col < xLength; col++) {
+                int x = sX + col;
+                int y = sY + row;
+                String img = "d" + d + "_x" + x + "_y" + y + ".png";
+                grid[row][col] = img;
+            }
+        }
+        return grid;
+    }
 }
